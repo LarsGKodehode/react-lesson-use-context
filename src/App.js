2b8f0f9d8eae076d5770564448ddc93933d279cf
LarsG <lars.ga.solheim@gmail.com>
2022-12-15 10:03:41 +0100

    first part of lesson, everything is inside a single file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState, createContext, useContext } from 'react';
+
+// CSS
 import './App.css';
 
+// Create context
+const OurContext = createContext(null)
+
+
 function App() {
+  const [ state, setState ] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OurContext.Provider value={state}>
+      <div className="App">
+        <Header />
+        <button onClick={() => setState((oldState) => !oldState)}>Change state</button>
+        <Main />
+      </div>
+    </OurContext.Provider>
   );
 }
+
+function Header() {
+  return (
+    <header>
+      <a>Homepage</a>
+    </header>
+  )
+}
+
+function Main() {
+  return (
+    <main>
+      <h1>Main Component</h1>
+      <Component1 />
+      <Component2 />
+    </main>
+  )
+}
+
+function Component1() {
+  return (
+    <div>
+      <h2>Component1</h2>
+    </div>
+  )
+}
+
+function Component2() {
+  const shouldRender = useContext(OurContext)
+
+  return (
+    <div>
+      <h2>Component2</h2>
+      {
+        shouldRender &&
+        <p>This should be conditionally rendered</p>
+      }
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+  return (
+    <main>
+      <h1>Some title</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </main>
+  )
+}
+
+export default Main
